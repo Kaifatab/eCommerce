e65b28b431a9ae335b82a0b5b289bad5923f9742
Kaifatab <TabassumKaifa2@gmail.com>
2023-05-22 21:22:24 +0600

    Dashboard private route

--- ECOMMERCE/client/src/App.js
+++ ECOMMERCE/client/src/App.js
@@ -6,12 +6,19 @@ import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/PageNotFound";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+          {/* <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} /> */}
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
