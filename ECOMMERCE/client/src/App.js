246a10a6487486293116b35716ee66de63179b0f
Md-Abdul-Kadir <abdulkaderakash08@gmail.com>
2023-06-14 00:24:26 +0600

    added code

--- ECOMMERCE/client/src/App.js
+++ ECOMMERCE/client/src/App.js
@@ -32,29 +32,29 @@ import AdminOrders from "./pages/Admin/AdminOrders";
 function App() {
   return (
     <>
-    <Navigation></Navigation>
+      <Navigation></Navigation>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<LandingPage />} />
-        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/product/:slug" element={<ProductDetails />} />
         <Route path="/category/:slug" element={<CategoryProduct />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/search" element={<Search />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="user" element={<Dashboard />} />
-          <Route path="user/orders" element={<Orders />} />
-          <Route path="user/profile" element={<Profile />} />
+        <Route path="user" element={<Dashboard />} />
+        <Route path="user/orders" element={<Orders />} />
+        <Route path="user/profile" element={<Profile />} />
         </Route>
         <Route path="/dashboard" element={<AdminRoute />}>
-          <Route path="admin" element={<AdminDashboard />} />
-          <Route path="admin/create-category" element={<CreateCatagory />} />
-          <Route path="admin/create-product" element={<CreateProduct />} />
-          <Route path="admin/product/:slug" element={<UpdateProduct />} />
-          <Route path="admin/products" element={<Products />} />
-          <Route path="admin/users" element={<Users />} />
-          <Route path="admin/orders" element={<AdminOrders />} />
+        <Route path="admin" element={<AdminDashboard />} />
+        <Route path="admin/create-category" element={<CreateCatagory />} />
+        <Route path="admin/create-product" element={<CreateProduct />} />
+        <Route path="admin/product/:slug" element={<UpdateProduct />} />
+        <Route path="admin/products" element={<Products />} />
+        <Route path="admin/users" element={<Users />} />
+        <Route path="admin/orders" element={<AdminOrders />} />
         </Route>
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/register" element={<Register />} />
@@ -64,7 +64,7 @@ function App() {
         <Route path="/policy" element={<Policy />} />
         <Route path="*" element={<Pagenotfound />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
